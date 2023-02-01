0d1580da24273fcb8dfc611f9b4e2870e3bf483d
Ahmed <ahmedeslam1582002@gmail.com>
2023-02-01 13:54:41 +0200

    First commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import connectDb from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use("/api/v1/post/", postRoutes);
+app.use("/api/v1/dalle/", dalleRoutes);
+app.get("/", async (req, res) => {
+  res.send("Hello from Ahmed!");
+});
+
+const startServer = async () => {
+  try {
+    connectDb(process.env.MONGODB_URL);
+    app.listen(8080, () =>
+      console.log("Server has started on port http://localhost:8080")
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+startServer();
+// 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
